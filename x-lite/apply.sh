0cab878dab6e294223b4dcb7f019d1162a593462
zen <didehdrb21@naver.com>
2025-09-19 17:04:10 +0900

    fix db_used variable

--- x-lite/apply.sh
+++ x-lite/apply.sh
@@ -6,6 +6,6 @@ DB_NAME=x-lite
 
 for file in init/*.sql; do
   echo "Applying $file..."
-  mysql -h $DB_HOST -u DB_USER -p$DB_PASS $DB_NAME < $file
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME < $file
 done
 
