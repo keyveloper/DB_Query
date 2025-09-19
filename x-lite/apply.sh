0f3830e9590aa706797999166e4073516145e55a
zen <didehdrb21@naver.com>
2025-09-19 16:27:00 +0900

    init script added

---
+++ x-lite/apply.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DB_HOST=localhost
+DB_USER=root
+DB_PASS=1234
+DB_NAME=x-lite
+
+for file in init/*.sql; do
+  echo "Applying $file..."
+  mysql -h $DB_HOST -u DB_USER -p$DB_PASS $DB_NAME < $file
+done
+
